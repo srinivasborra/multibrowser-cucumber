89bd87d97bbc13b9f6be5f6c771179650f71bd7f
Srinivas Borra <nborra@apple.com>
2018-10-01 22:27:55 -0700

    Image Comparision

---
+++ src/test/example/ImageComparision.java
@@ -0,0 +1,29 @@
+package test.example;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.File;
+import java.util.Arrays;
+
+import javax.imageio.ImageIO;
+
+public class ImageComparision {
+	public static void main(String arr[]){
+		try{
+			BufferedImage image1 = ImageIO.read(new File("images/logo_1.png"));
+			BufferedImage image2 = ImageIO.read(new File("images/logo_2.png"));
+			
+			byte[] pixel1 = ((DataBufferByte) image1.getRaster().getDataBuffer()).getData();
+			
+			byte[] pixel2 = ((DataBufferByte) image2.getRaster().getDataBuffer()).getData();
+
+			if(Arrays.equals(pixel1,pixel2)){
+				System.out.println("Images are equal");
+			}else{
+				System.out.println("Images are not equal");
+			}
+		}catch(Exception ex){}
+	
+
+}	
+}
